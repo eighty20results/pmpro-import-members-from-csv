1d0f5c107a33c1546c8a031de4d8a066e074c1a0
Thomas Sjolshagen <thomas@eighty20results.com>
2021-05-31 09:38:56 +0200

    BUG FIX: Fix PHPCS warnings

--- tests/unit/ImportMembers_UnitTest.php
+++ tests/unit/ImportMembers_UnitTest.php
@@ -27,45 +27,45 @@ use E20R\Test\Unit\Test_In_A_Tweet\TFIAT;
 // Functions to import from other namespaces
 use function E20R\Test\Unit\Fixtures\plugin_row_meta_data;
 use function PHPUnit\Framework\assertEquals;
-
+use function Brain\Monkey\Functions\stubEscapeFunctions;
 
 class ImportMembers_UnitTest extends TFIAT {
-	
+
 	/**
 	 * Load all function mocks we need (with namespaces)
 	 */
 	public function loadMocks() : void {
-		
+
 		Functions\when( 'esc_url' )
 			->justReturn( 'https://localhost:7537' );
-		
+
 		Functions\when( 'esc_url_raw' )
 			->justReturn( 'https://localhost:7537' );
-		
+
 		Functions\when( 'esc_attr__' )
 			->returnArg( 1 );
-		
-		Functions\when('get_transient' )
+
+		Functions\when( 'get_transient' )
 			->justReturn( '/var/www/html/wp-content/uploads/e20r_import/example_file.csv' );
-		
+
 		Functions\when( 'wp_upload_dir' )
 			->justReturn(
 				array(
 					'baseurl' => 'https://localhost:7537/wp-content/uploads/',
-					'basedir' => '/var/www/html/wp-content/uploads'
+					'basedir' => '/var/www/html/wp-content/uploads',
 				)
 			);
 		Functions\expect( 'get_option' )
-#			->with( 'e20r_link_for_sponsor' )
-#			->once()
+			->with( 'e20r_link_for_sponsor' )
+			->once()
 			->andReturn( 'https://www.paypal.com/cgi-bin/webscr' );
-		
+
 		Functions\expect( 'update_option' )
-			->andReturn( true );
-#			->with( 'e20r_link_for_sponsor' );
-#			->once();
+			->andReturn( true )
+			->with( 'e20r_link_for_sponsor' )
+			->once();
 	}
-	
+
 	/**
 	 * Load all needed source files for the unit test
 	 */
@@ -81,31 +81,32 @@ class ImportMembers_UnitTest extends TFIAT {
 		require_once __DIR__ . BASE_SRC_PATH . '/class.pmpro-import-members.php';
 		require_once __DIR__ . BASE_SRC_PATH . '/inc/autoload.php';
 	}
-	
+
 	/**
 	 * Define function stubs for the unit test
 	 */
 	public function loadStubs() : void {
 		Functions\stubs(
-			[
-				'esc_attr',
-				'esc_html',
-				'esc_textarea',
-				'__',
-				'_x',
-				'esc_html__',
-				'esc_html_x',
-				'esc_attr_x',
-				'plugin_dir_path' => function() { return '/var/www/html/wp-content/plugins/pmpro-import-members-from-csv'; },
-				'esc_url'  => function() { return 'https://localhost:7537'; },
-				'esc_url_raw' => function() { return 'https://localhost:7537'; },
-				'get_transient' => function() { return '/var/www/html/wp-content/uploads/e20r_import/example_file.csv'; },
-				'wp_upload_dir' => function() { return array(
-					'baseurl' => 'https://localhost:7537/wp-content/uploads/',
-				);
+			array(
+				'plugin_dir_path'            => function() {
+					return '/var/www/html/wp-content/plugins/pmpro-import-members-from-csv';
 				},
-				'register_deactivation_hook' => '__return_true'
-			]
+				'esc_url'                    => function() {
+					return 'https://localhost:7537';
+				},
+				'esc_url_raw'                => function() {
+					return 'https://localhost:7537';
+				},
+				'get_transient'              => function() {
+					return '/var/www/html/wp-content/uploads/e20r_import/example_file.csv';
+				},
+				'wp_upload_dir'              => function() {
+					return array(
+						'baseurl' => 'https://localhost:7537/wp-content/uploads/',
+					);
+				},
+				'register_deactivation_hook' => '__return_true',
+			)
 		);
 	}
 }
@@ -120,16 +121,15 @@ $bm = new ImportMembers_UnitTest();
 $bm->it(
 	'should test that we generate the expected plugin metadata',
 	function () {
-		
+
 		$fixture_list = plugin_row_meta_data();
-		
+
 		foreach ( $fixture_list as $fixture ) {
 			list( $row_meta_list, $file_name, $expected_result ) = $fixture;
 			$class    = Import_Members::get_instance();
 			$row_list = $class->plugin_row_meta( $row_meta_list, $file_name );
-			$result = \count( $row_list );
+			$result   = \count( $row_list );
 			assertEquals( $expected_result, $result );
 		}
 	}
 );
-// $bm->tearDown();
