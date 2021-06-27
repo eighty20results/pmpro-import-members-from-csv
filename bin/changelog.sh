3cc513d84e3f8762857b89338862d5a0c0d972fc
Thomas Sjolshagen <thomas@eighty20results.com>
2021-06-27 22:07:08 +0200

    BUG FIX: Clean up changelog management

--- bin/changelog.sh
+++ bin/changelog.sh
@@ -8,9 +8,9 @@ changelog_out="CHANGELOG.md"
 wordpress_version=$(wget -q -O - http://api.wordpress.org/core/stable-check/1.0/  | grep latest | awk '{ print $1 }' | sed -e 's/"//g')
 tmp_changelog=$(mktemp /tmp/chlog-XXXXXX)
 # stripped_log=$(mktemp /tmp/old-info-XXXXXX)
-version=$(egrep "^Version:" ./class.${short_name}.php | sed 's/[[:alpha:]|(|[:space:]|\:]//g' | awk -F- '{printf "%s", $1}')
-today=$(date "+%Y-%m-%d")
-changelog_new_version="## [${version}] - ${today}"
+version=$(grep -E "^Version:" ./class.pmpro-import-members.php | sed 's/[[:alpha:]|(|[:space:]|\:]//g' | awk -F- '{printf "%s", $1}')
+today=$(date +%Y-%m-%d)
+changelog_new_version="## v${version} - ${today}"
 changelog_header=$(cat <<- __EOF__
 # Changelog
 All notable changes to this project will be documented in this file.
@@ -24,23 +24,23 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 __EOF__
 )
 
-if [[ ! -f ./metadata.json ]]; then
-	cp "${readme_path}/metadata.json" metadata.json
-fi
+#if [[ ! -f ./metadata.json ]]; then
+#	cp "${readme_path}/skel/metadata.json" ./metadata.json
+#fi
 
 ###########
 #
 # Update plugin and wordpress version info in metadata.json
 #
-if [[ -f ./metadata.json ]]; then
-	echo "Updating the metadata.json file"
-	"${sed}" -r -e "s/\"version\": \"([0-9]+\.[0-9].*)\"\,/\"version\": \"${version}\"\,/" \
-					 -e "s/\"tested\"\:\ \"([0-9]+\.[0-9].*)\"\,/\"tested\"\:\ \"${wordpress_version}\"\,/" \
-					 -e "s/\"last_updated\": \"(.*)\",/\"last_updated\": \"${today} $(date \"+%h:%m:00\") CET\",/g" \
-					 -e "s/\"download_url\": \"https:\/\/${server}\/protected-content\/${short_name}\/${short_name}-([0-9]+\.[0-9].*)\.zip\",/\"download_url\": \"https:\/\/${server}\/protected-content\/${short_name}\/${short_name}-${version}\.zip\",/g" \
-					 ./metadata.json > ./new_metadata.json
-		mv ./new_metadata.json ./metadata.json
-fi
+#if [[ -f ./metadata.json ]]; then
+#	echo "Updating the metadata.json file"
+#	"${sed}" -r -e "s/\"version\": \"([0-9]+\.[0-9].*)\"\,/\"version\": \"${version}\"\,/" \
+#					 -e "s/\"tested\"\:\ \"([0-9]+\.[0-9].*)\"\,/\"tested\"\:\ \"${wordpress_version}\"\,/" \
+#					 -e "s/\"last_updated\": \"(.*)\",/\"last_updated\": \"${today} $(date +%H:%M:00) CET\",/g" \
+#					 -e "s/\"download_url\": \"https:\/\/${server}\/protected-content\/${short_name}\/${short_name}-([0-9]+\.[0-9].*)\.zip\",/\"download_url\": \"https:\/\/${server}\/protected-content\/${short_name}\/${short_name}-${version}\.zip\",/g" \
+#					 ./metadata.json > ./new_metadata.json
+#		mv ./new_metadata.json ./metadata.json
+#fi
 
 ###########
 #
@@ -76,9 +76,9 @@ if ! grep "${changelog_new_version}" "${changelog_out}"; then
 	# Add dash (-) to all entries in the changelog source for the new CHANGELOG.md file
 	"${sed}" -e"s/\"/\'/g" -e"s/.*/-\ &/" "${changelog_source}" >> "./${changelog_out}"
 	# Append the old change log to the new file
-	cat "./${tmp_changelog}" >> "./${changelog_out}"
+	cat "${tmp_changelog}" >> "./${changelog_out}"
 	# Clean up temp file(s)
-	rm -f "./${tmp_changelog}"
+	rm -f "${tmp_changelog}"
 fi
 
-git commit -m "Updated version info during build (v${version} for WP ${wordpress_version})" CHANGELOG.md README.txt metadata.json
+git commit -m "Updated version info during build (v${version} for WP ${wordpress_version})" CHANGELOG.md README.txt # metadata.json
