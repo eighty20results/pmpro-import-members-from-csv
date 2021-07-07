f15177f5ad25afdaa332f658c8319d568b96bd91
Thomas Sjolshagen <thomas@eighty20results.com>
2021-07-07 15:06:45 +0200

    BUG FIX: Fatal error because we attempted to use Data::get_instance() plus PHPCS nits

--- src/modules/PMPro/class-sponsor.php
+++ src/modules/PMPro/class-sponsor.php
@@ -19,32 +19,31 @@
 
 namespace E20R\Import_Members\Modules\PMPro;
 
-
 use E20R\Import_Members\Data;
 
 class Sponsor {
-	
+
 	/**
 	 * The sponsor's WP_User ID
 	 *
 	 * @var null|int $ID
 	 */
 	private $ID = null;
-	
+
 	/**
 	 * User Record for the sponsor
 	 *
 	 * @var null|\WP_User  $user
 	 */
 	private $user = null;
-	
+
 	/**
 	 * The membership level information for the sponsor
 	 *
 	 * @var null|\stdClass $membership_level
 	 */
 	private $membership_level = null;
-	
+
 	/**
 	 * Sponsor constructor.
 	 *
@@ -53,18 +52,24 @@ class Sponsor {
 	 * @throws \Exception
 	 */
 	public function __construct( $user_id = null ) {
-		
-		$data = Data::get_instance();
-		
+
+		$data = new Data();
+
 		if ( ! empty( $user_id ) ) {
 			$this->user = $data->get_user_info( $user_id );
 		}
-		
-		if ( !empty( $user_id ) && empty( $this->user ) ) {
-			throw new \Exception( sprintf( __( 'No user with that ID (%s) found!', 'pmpro-import-members-from-csv' ), $user_id ) );
+
+		if ( ! empty( $user_id ) && empty( $this->user ) ) {
+			throw new \Exception(
+				sprintf(
+					// translators: %s - Supplied User ID, email or login name
+					__( 'No user with that ID (%s) found!', 'pmpro-import-members-from-csv' ),
+					$user_id
+				)
+			);
 		}
 	}
-	
+
 	/**
 	 * Set Sponsor parameter by type
 	 *
@@ -75,15 +80,15 @@ class Sponsor {
 	 * @return bool
 	 */
 	public function set( $param, $value, $type = null ) {
-		
+
 		$attributes = get_class_vars( __CLASS__ );
-		
-		if ( ! in_array( $param, $attributes ) && ! in_array( $type, $attributes ) ) {
+
+		if ( ! in_array( $param, $attributes, true ) && ! in_array( $type, $attributes, true ) ) {
 			return false;
 		}
-		
+
 		switch ( $type ) {
-			
+
 			case 'user':
 				if ( isset( $this->user->{$param} ) ) {
 					$this->user->{$param} = $value;
@@ -91,23 +96,22 @@ class Sponsor {
 					return false;
 				}
 				break;
-			
+
 			case 'membership':
-				
 				if ( isset( $this->membership_level->{$param} ) ) {
 					$this->membership_level->{$param} = $value;
 				} else {
 					return false;
 				}
 				break;
-			
+
 			default:
 				$this->{$param} = $value;
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Return the type specific parameter (membership info, user info, class variable)
 	 *
@@ -117,29 +121,28 @@ class Sponsor {
 	 * @return bool|mixed|null
 	 */
 	public function get( $type, $param ) {
-		
+
 		$attributes = get_class_vars( __CLASS__ );
 		$value      = null;
-		
-		if ( ! in_array( $param, $attributes ) ) {
+
+		if ( ! in_array( $param, $attributes, true ) ) {
 			return false;
 		}
-		
+
 		switch ( $type ) {
-			
+
 			case 'user':
 				$value = $this->user->{$param};
 				break;
-			
+
 			case 'membership':
-				
 				$value = $this->membership_level->{$param};
 				break;
-			
+
 			default:
 				$value = $this->{$param};
 		}
-		
+
 		return $value;
 	}
 }
