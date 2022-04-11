9e609af42b6037c103f0991a3de68d9070bcc649
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-04-11 11:56:22 +0200

    BUG FIX: Using composer autoloader and handling PHPStan testing better

--- class.pmpro-import-members.php
+++ class.pmpro-import-members.php
@@ -35,16 +35,16 @@ namespace E20R\Import;
 
 use E20R\Import_Members\Import;
 use E20R\Utilities\ActivateUtilitiesPlugin;
-use RecursiveCallbackFilterIterator;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use function plugin_dir_path;
 use function add_action;
-use function error_log; // phpcs:ignore
+use function register_deactivation_hook;
 
 // Load the PSR-4 Autoloader
 require_once __DIR__ . '/inc/autoload.php';
 require_once __DIR__ . '/ActivateUtilitiesPlugin.php';
+
+if ( ! defined( 'E20R_IMPORT_PLUGIN_FILE' ) ) {
+	define( 'E20R_IMPORT_PLUGIN_FILE', __FILE__ );
+}
 
 if ( ! defined( 'E20R_IM_CSV_DELIMITER' ) ) {
 	define( 'E20R_IM_CSV_DELIMITER', ',' );
@@ -69,97 +69,6 @@ if ( ! class_exists( '\E20R\Import\Loader' ) ) {
 	class Loader {
 
 		/**
-		 * Class auto-loader
-		 *
-		 * @param string $class_name Name of the class to auto-load
-		 *
-		 * @since  1.0
-		 * @access public static
-		 *
-		 * @since  v2.2 - BUG FIX: Include blocks/ directory to auto_loader() method
-		 * @since v3.0 - BUG FIX: Refactored to only contain the autoloader method
-		 */
-		public static function auto_loader( $class_name ) {
-
-			if ( false === stripos( $class_name, 'e20r' ) ) {
-				return;
-			}
-
-			$parts      = explode( '\\', $class_name );
-			$c_name     = strtolower( preg_replace( '/_/', '-', $parts[ ( count( $parts ) - 1 ) ] ) );
-			$base_paths = array();
-
-			if ( file_exists( plugin_dir_path( __FILE__ ) . 'src/' ) ) {
-				$base_paths[] = plugin_dir_path( __FILE__ ) . 'src/';
-			}
-
-			if ( file_exists( plugin_dir_path( __FILE__ ) . 'classes/' ) ) {
-				$base_paths[] = plugin_dir_path( __FILE__ ) . 'classes/';
-			}
-
-			if ( file_exists( plugin_dir_path( __FILE__ ) . 'class/' ) ) {
-				$base_paths[] = plugin_dir_path( __FILE__ ) . 'class/';
-			}
-
-			if ( file_exists( plugin_dir_path( __FILE__ ) . 'blocks/' ) ) {
-				$base_paths[] = plugin_dir_path( __FILE__ ) . 'blocks/';
-			}
-			$filename = "class-{$c_name}.php";
-
-			foreach ( $base_paths as $base_path ) {
-
-				try {
-					$iterator = new RecursiveDirectoryIterator(
-						$base_path,
-						RecursiveDirectoryIterator::SKIP_DOTS |
-						RecursiveIteratorIterator::SELF_FIRST |
-						RecursiveIteratorIterator::CATCH_GET_CHILD |
-						RecursiveDirectoryIterator::FOLLOW_SYMLINKS
-					);
-				} catch ( \Exception $e ) {
-					print 'Error: ' . $e->getMessage(); // phpcs:ignore
-
-					return;
-				}
-
-				try {
-					$filter = new RecursiveCallbackFilterIterator(
-						$iterator,
-						function ( $current, $key, $iterator ) use ( $filename ) {
-
-							// Skip hidden files and directories.
-							if ( '.' === $current->getFilename()[0] || '..' === $current->getFilename() ) {
-								return false;
-							}
-
-							if ( $current->isDir() ) {
-								// Only recurse into intended subdirectories.
-								return $current->getFilename() === $filename;
-							} else {
-								// Only consume files of interest.
-								return str_starts_with( $current->getFilename(), $filename );
-							}
-						}
-					);
-				} catch ( \Exception $e ) {
-					echo 'Autoloader error: ' . $e->getMessage(); // phpcs:ignore
-
-					return;
-				}
-
-				foreach ( new RecursiveIteratorIterator( $iterator ) as $f_filename => $f_file ) {
-
-					$class_path = $f_file->getPath() . '/' . $f_file->getFilename();
-
-					if ( $f_file->isFile() && false !== stripos( $class_path, $filename ) ) {
-
-						require_once $class_path;
-					}
-				}
-			}
-		}
-
-		/**
 		 * Validates that the E20R utilities module is available and active (and attempts to activate it if not)
 		 */
 		public static function is_utilities_module_active() {
@@ -177,27 +86,8 @@ if ( ! class_exists( '\E20R\Import\Loader' ) ) {
 	}
 }
 
-if ( ! defined( 'E20R_IMPORT_PLUGIN_FILE' ) ) {
-	define( 'E20R_IMPORT_PLUGIN_FILE', __FILE__ );
+if ( defined( 'ABSPATH' ) && ! defined( 'PLUGIN_PHPUNIT' ) ) {
+	register_deactivation_hook( __FILE__, 'E20R\\Import_Members\\Import::deactivation' );
+	// Load this plugin
+	add_action( 'plugins_loaded', array( new Import(), 'load_hooks' ), 10 );
 }
-
-// Register the auto-loader for this plugin
-//try {
-//	spl_autoload_register( '\E20R\Import\Loader::auto_loader' );
-//} catch ( \Exception $exception ) {
-//	// phpcs:ignore
-//	error_log(
-//		__(
-//			'Unable to load E20R autoloader for the PMPro Import Members from CSV plugin!',
-//			'pmpro-import-members-from-csv'
-//		),
-//		E_USER_ERROR
-//	);
-//
-//	return;
-//}
-
-\register_deactivation_hook( __FILE__, 'E20R\\Import_Members\\Import::deactivation' );
-
-// Load this plugin
-add_action( 'plugins_loaded', array( Import::get_instance(), 'load_hooks' ), 10 );
