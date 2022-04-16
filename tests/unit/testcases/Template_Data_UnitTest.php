41a0e8259bc37c6fbbd274be172b043bf5e8d38e
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-04-16 22:44:45 +0200

    BUG FIX: Broken unit testing for Template_Data()

--- tests/unit/testcases/Template_Data_UnitTest.php
+++ tests/unit/testcases/Template_Data_UnitTest.php
@@ -18,15 +18,19 @@
  */
 namespace E20R\Tests\Unit;
 
+use Brain\Monkey;
 use Brain\Monkey\Functions;
 use Codeception\Test\Unit;
-use Brain\Monkey;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery;
+use stdClass;
+use WP_Mock;
+
 
 use E20R\Import_Members\Email\Template_Data;
 use E20R\Import_Members\Error_Log;
-use function Brain\faker;
-
+use WP_User;
+use MemberOrder;
 
 /**
  * Class Imported_Data_Test
@@ -34,34 +38,41 @@ use function Brain\faker;
  */
 class Template_Data_UnitTest extends Unit {
 
-	use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+	use MockeryPHPUnitIntegration;
 
 	/**
-	 * @see https://github.com/Brain-WP/BrainFaker
+	 * Mocked Error_Log() class
+	 *
+	 * @var null|Error_Log
 	 */
-	protected $wp_faker;
-
-	private $faker;
+	private $errlog_mock = null;
 
 	/**
-	 * Codeception _before() method
+	 * Mocked PMPro MemberOrder()
+	 *
+	 * @var null|MemberOrder
 	 */
-	public function setUp() : void {  //phpcs:ignore
+	private $mock_order = null;
+
+	/**
+	 * Codeception setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp() : void {
 		parent::setUp();
+		WP_Mock::setUp();
 		Monkey\setUp();
 		$this->loadMocks();
-		$this->loadTestSources();
-
-		$this->faker    = faker();
-		$this->wp_faker = $this->faker->wp();
-
 	}
 
 	/**
-	 * Codeception _after() method
+	 * Codeception tearDown() method
+	 *
+	 * @return void
 	 */
-	public function tearDown() : void { //phpcs:ignore
-		\Brain\fakerReset();
+	public function tearDown() : void {
+		WP_Mock::tearDown();
 		Monkey\tearDown();
 		parent::tearDown();
 	}
@@ -70,6 +81,26 @@ class Template_Data_UnitTest extends Unit {
 	 * Load all function mocks we need (with namespaces)
 	 */
 	public function loadMocks() : void {
+
+		$this->errlog_mock = $this->makeEmpty(
+			Error_Log::class,
+			array(
+				'debug' => function( $msg ) {
+					// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+					error_log( "Mocked log: {$msg}" );
+				},
+			)
+		);
+
+		$this->mock_order = $this->makeEmpty(
+			MemberOrder::class,
+			array(
+				'getLastMemberOrder' => $this->fixture_pmpro_order(),
+				'setGateway'         => false,
+				'getRandomCode'      => $this->random_strings( 10 ),
+				'get_test_order'     => $this->fixture_pmpro_order(),
+			)
+		);
 
 		Functions\when( 'esc_url' )
 			->justReturn( 'https://localhost:7537' );
@@ -91,26 +122,16 @@ class Template_Data_UnitTest extends Unit {
 	}
 
 	/**
-	 * Load all needed source files for the unit test
-	 */
-	public function loadTestSources(): void {
-		require_once __DIR__ . '/../../../inc/autoload.php';
-		require_once __DIR__ . '/../../../inc/wp_plugins/paid-memberships-pro/classes/class.memberorder.php';
-	}
-
-	/**
 	 * Validates the Template_Data class and it's methods and attributes
 	 *
+	 * @param array $methods List of methods to look for
+	 * @param array $attributes List of class properties to look for
+	 * @param array $instantiated_classes List of classes to look for
+	 *
 	 * @dataProvider fixture_template_data_create
+	 * @test
 	 */
-	public function test_template_data_create( $methods, $attributes, $instantiated_classes ) {
-
-		$errlog_mock = $this->getMockBuilder( Error_Log::class )
-							->onlyMethods( array( 'debug' ) )
-							->getMock();
-
-		$errlog_mock->method( 'debug' )
-					->willReturn( null );
+	public function it_should_validate_template_data_class( $methods, $attributes, $instantiated_classes ) {
 
 		try {
 			Functions\expect( 'get_option' )
@@ -120,7 +141,22 @@ class Template_Data_UnitTest extends Unit {
 			echo 'Error: ' . $e->getMessage(); // phpcs:ignore
 		}
 
-		$template_data_class = new Template_Data();
+		Functions\when( 'apply_filters' )
+			->returnArg( 1 );
+
+
+		$user      = $this->create_mocked_wp_users( 1 );
+		$user_meta = $this->fixture_user_meta();
+
+		Functions\when( 'wp_get_current_user' )
+			->justReturn( $user );
+
+		Functions\when( 'get_user_meta' )
+			->justReturn( $user );
+
+		$test_order = $this->mock_order->get_test_order( $user );
+
+		$template_data_class = new Template_Data( $test_order, $user, $user_meta, $this->errlog_mock );
 		$this->assertInstanceOf( 'E20R\\Import_Members\\Email\\Template_Data', $template_data_class );
 
 		foreach ( $instantiated_classes as $namespaced_class => $class_name ) {
@@ -149,7 +185,7 @@ class Template_Data_UnitTest extends Unit {
 		$methods              = array( 'configure_objects', 'default_site_field_values', 'get', 'load_field_aliases' );
 		$attributes           = array( 'user', 'order', 'meta', 'field_aliases', 'site_fields', 'error_log' );
 		$instantiated_classes = array(
-			'E20R\\Import_Members\\Error_Log' => 'error_log',
+			'E20R\Import_Members\Error_Log' => 'error_log',
 		);
 
 		return array(
@@ -161,22 +197,25 @@ class Template_Data_UnitTest extends Unit {
 	 * Test whether the correct path is returned for the Import file specified
 	 * (no user, order or user meta data supplied in constructor)
 	 *
+	 * @param WP_User $user The mocked user
+	 * @param string  $site_name The name of the site
+	 * @param string  $wp_login_url The URL to the site's login page
+	 * @param string  $levels_link The URL to the site's levels settings page
+	 * @param string  $from_email The email address for the user
+	 * @param string  $display_name The display_name for the user
+	 * @param array   $default_site_field_keys The keys to use/test for
+	 * @param mixed   $expected_results The expected test results
+	 *
 	 * @dataProvider fixture_default_site_field_values
+	 * @test
 	 */
-	public function test_default_site_field_values( $user, $site_name, $wp_login_url, $levels_link, $from_email, $display_name, $default_site_field_keys, $expected_results ) {
+	public function it_should_set_default_site_field_values( $user, $site_name, $wp_login_url, $levels_link, $from_email, $display_name, $default_site_field_keys, $expected_results ) {
 
 		if ( ! empty( $user ) ) {
 			global $current_user;
 			// For testing purposes
 			$current_user = $user; // phpcs:ignore
 		}
-
-		$errlog_mock = $this->getMockBuilder( Error_Log::class )
-							->onlyMethods( array( 'debug' ) )
-							->getMock();
-
-		$errlog_mock->method( 'debug' )
-					->willReturn( null );
 
 		try {
 			Functions\expect( 'get_option' )
@@ -197,8 +236,15 @@ class Template_Data_UnitTest extends Unit {
 			->with( Mockery::contains( 'from_email' ) )
 			->andReturn( $from_email );
 
+		Functions\when( 'wp_get_current_user' )
+			->justReturn( $user );
+
+		Functions\when( 'get_user_meta' )
+			->justReturn( $user );
+
 		// Instantiate our class
-		$imported_data = new Template_Data();
+		$test_order    = $this->mock_order->get_test_order( $user );
+		$imported_data = new Template_Data( $test_order, $user, $this->fixture_user_meta() );
 		$result        = $imported_data->default_site_field_values();
 
 		// Make sure the returned data is an array (list)
@@ -222,9 +268,9 @@ class Template_Data_UnitTest extends Unit {
 	 *
 	 * @see https://github.com/Brain-WP/BrainFaker
 	 */
-	public function fixture_default_site_field_values() : array {
+	public function fixture_default_site_field_values() {
 
-		$user_list               = $this->wp_faker->user( 5 );
+		$user_list               = $this->create_mocked_wp_users( 5 );
 		$default_site_field_keys = array( 'sitename', 'siteemail', 'login_link', 'levels_link', 'name' );
 		$fixture                 = array();
 
@@ -233,22 +279,63 @@ class Template_Data_UnitTest extends Unit {
 			$fixture[] = array(
 				$user,
 				'Test Site',
-				'http://mytestwebsite.com/wp-login.php',
-				'http://mytestwebsite.com/membership/membership_levels/',
+				'https://mytestwebsite.com/wp-login.php',
+				'https://mytestwebsite.com/membership/membership_levels/',
 				'admin@mytestwebsite.com',
 				$user->display_name,
 				$default_site_field_keys,
 				array(
 					'sitename'    => 'Test Site',
 					'siteemail'   => 'admin@mytestwebsite.com',
-					'login_link'  => 'http://mytestwebsite.com/wp-login.php',
-					'levels_link' => 'http://mytestwebsite.com/membership/membership_levels/',
+					'login_link'  => 'https://mytestwebsite.com/wp-login.php',
+					'levels_link' => 'https://mytestwebsite.com/membership/membership_levels/',
 					'name'        => $user->display_name,
 				),
 			);
 		}
 
 		return $fixture;
+	}
+
+	/**
+	 * Create mocked WP_User objects
+	 *
+	 * @param int $count The number of mocked WP_User objects to create and return
+	 *
+	 * @return array
+	 */
+	private function create_mocked_wp_users( $count = 1 ) {
+		$user_list = array();
+		$base_id   = 1000;
+
+		for ( $i = 0; $i < $count; $i++ ) {
+			$id = ( $base_id + $i );
+
+			if ( class_exists( '\WP_User' ) ) {
+				$user = $this->construct( WP_User::class,
+				array( $id )
+				);
+			} else {
+				$user     = new stdClass();
+				$user->ID = $id;
+			}
+
+			$user->user_firstname   = 'Test';
+			$user->first_name       = $user->user_firstname;
+			$user->user_lastname    = "User # {$id}";
+			$user->last_name        = $user->user_lastname;
+			$user->user_description = "{$user->user_firstname} {$user->user_lastname}";
+			$user->display_name     = "{$user->user_firstname} {$user->user_lastname}";
+			$user->user_email       = "test_{$id}@localhost.local";
+
+			if ( $count > 1 ) {
+				$user_list[] = $user;
+			} else {
+				$user_list = $user;
+			}
+		}
+
+		return $user_list;
 	}
 
 	/**
@@ -274,7 +361,7 @@ class Template_Data_UnitTest extends Unit {
 	 *
 	 * @param int $length_of_string
 	 *
-	 * @return false|string
+	 * @return string
 	 */
 	private function random_strings( $length_of_string ) {
 		// String of all alphanumeric character
@@ -290,9 +377,16 @@ class Template_Data_UnitTest extends Unit {
 	}
 
 	/**
+	 * Test of the Template_Data() class when user meta has been saved
+	 *
+	 * @param WP_User     $user User object from PMPro
+	 * @param \MemberOrder $order PMPro Member Order object
+	 * @param array       $user_meta WP User Metadata
 	 * @dataProvider fixture_configure_objects
+	 *
+	 * @test
 	 */
-	public function test_configure_objects( $user, $order, $user_meta ) {
+	public function it_should_configure_objects_for_template( $user, $order, $user_meta ) {
 
 		Functions\expect( 'pmpro_getOption' )
 			->with( Mockery::contains( 'from_email' ) )
@@ -312,28 +406,24 @@ class Template_Data_UnitTest extends Unit {
 		Functions\when( 'get_current_user' )
 			->justReturn( $user );
 
-		$imported_data = new Template_Data();
+		Functions\when( 'wp_get_current_user' )
+			->justReturn( $user );
 
-		$this->assertEquals( null, $imported_data->get( 'ID', 'user' ) );
-		$this->assertEquals( null, $imported_data->get( 'display_name', 'user' ) );
-		$this->assertEquals( null, $imported_data->get( 'id', 'order' ) );
-		$this->assertEquals( null, $imported_data->get( 'membership_id', 'meta' ) );
+		$imported_data = new Template_Data( $order, $user, $user_meta, $this->errlog_mock );
 
-		$imported_data->configure_objects( $user, $order, $user_meta );
-
-		$this->assertEquals( $user->id, $imported_data->get( 'ID', 'user' ) );
+		$this->assertEquals( $user->ID, $imported_data->get( 'ID', 'user' ) );
 		$this->assertEquals( $user->display_name, $imported_data->get( 'display_name', 'user' ) );
 		$this->assertEquals( $order->id, $imported_data->get( 'id', 'order' ) );
 		$this->assertEquals( $user_meta['membership_id'], $imported_data->get( 'membership_id', 'meta' ) );
 	}
 
 	/**
-	 * Fixture to generate User, MemberOrder and user metadata objects/arrays
+	 * Fixture to generate User, \MemberOrder and user metadata objects/arrays
 	 *
 	 * @return array[]
 	 */
 	public function fixture_configure_objects() {
-		$user_list = $this->wp_faker->atLeastFiveUsers();
+		$user_list = $this->create_mocked_wp_users( 5 );
 		$user_meta = $this->fixture_user_meta();
 		$fixture   = array();
 
@@ -349,36 +439,38 @@ class Template_Data_UnitTest extends Unit {
 	/**
 	 * Generate mock/fake PMPro MemberOrder object
 	 *
-	 * @param \WP_User $user
+	 * @param \WP_User|null $user User information
+	 *
 	 * @return \MemberOrder
 	 */
-	private function fixture_pmpro_order( $user ) {
+	public function fixture_pmpro_order( $user = null ) {
 
-		$order = $this->getMockBuilder( \MemberOrder::class )
-		              ->onlyMethods( array( 'getLastMemberOrder', 'setGateway', 'getRandomCode' ) )
-		              ->getMock();
+		if ( null === $user ) {
+			$user = $this->create_mocked_wp_users( 1 );
+		}
 
-		$order->method( 'setGateway' )
-		      ->willReturn( false );
+		if ( empty( $this->mock_order ) ) {
+			$this->mock_order = $this->construct(
+				MemberOrder::class,
+				array(),
+				array(
+					'getLastMemberOrder' => $this->mock_order,
+					'setGateway'         => false,
+					'getRandomCode'      => $this->random_strings( 10 ),
+					'get_test_order'     => $this->mock_order,
+				)
+			);
+		}
+		$this->mock_order->set( 'id', 1 );
+		$this->mock_order->set( 'user_id', $user->ID );
+		$this->mock_order->set( 'FirstName', $user->first_name );
+		$this->mock_order->set( 'LastName', $user->last_name ); // phpcs:ignore
+		$this->mock_order->set( 'Email', $user->user_email ); // phpcs:ignore
+		$this->mock_order->set( 'membership_id', 1 );
+		$this->mock_order->set( 'billing', '' );
+		$this->mock_order->set( 'billing_name', $user->display_name );
+		$this->mock_order->setGateway( 'stripe' );
 
-		$order->method( 'getRandomCode' )
-		      ->willReturn(
-			      $this->random_strings( 10 )
-		      );
-
-		$order->method( 'getLastMemberOrder' )
-		      ->willReturn( $order );
-
-		$order->id            = $order->getRandomCode();
-		$order->user_id       = $user->ID;
-		$order->FirstName     = $user->first_name; // phpcs:ignore
-		$order->LastName      = $user->last_name; // phpcs:ignore
-		$order->Email         = $user->user_email; // phpcs:ignore
-		$order->membership_id = 1;
-		$order->billing       = new \stdClass();
-		$order->billing->name = $user->display_name;
-		$order->setGateway( 'stripe' );
-
-		return $order;
+		return $this->mock_order;
 	}
 }
