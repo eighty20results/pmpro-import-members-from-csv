2845598bac5fdb7e79e5d91ffb2b42b04b40801e
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-04-18 22:20:41 +0200

    BUG FIX: Transition from singleton/static functions to class/method and use Status() values for errors/issues

--- src/E20R/Import_Members/Modules/Users/User_Update.php
+++ src/E20R/Import_Members/Modules/Users/User_Update.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2018 - 2021. - Eighty / 20 Results by Wicked Strong Chicks.
+ * Copyright (c) 2018 - 2022. - Eighty / 20 Results by Wicked Strong Chicks.
  * ALL RIGHTS RESERVED
  *
  * This program is free software: you can redistribute it and/or modify
@@ -15,18 +15,54 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package E20R\Import_Members\Modules\User_Update
  */
 
 namespace E20R\Import_Members\Modules\Users;
 
+use E20R\Exceptions\InvalidSettingsKey;
+use E20R\Import_Members\Error_Log;
+use E20R\Import_Members\Status;
 use E20R\Import_Members\Variables;
 
-if ( ! class_exists( '\E20R\Import_Members\Modules\Users' ) ) {
+if ( ! class_exists( '\E20R\Import_Members\Modules\User_Update' ) ) {
 	/**
 	 * Class User_Update
-	 * @package E20R\Import_Members\Modules\Users
 	 */
 	class User_Update {
+
+		/**
+		 * Instance of the Error_Log() class
+		 *
+		 * @var Error_Log|null
+		 */
+		private $error_log = null;
+
+		/**
+		 * Instance of the Variables() class
+		 *
+		 * @var Variables|null
+		 */
+		private $variables = null;
+
+		/**
+		 * Constructor for the User_Update() class
+		 *
+		 * @param Variables|null $variables
+		 * @param Error_Log|null $error_log
+		 */
+		public function __construct( &$variables = null, &$error_log = null ) {
+			if ( null === $error_log ) {
+				$error_log = new Error_Log(); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			}
+			$this->error_log = $error_log;
+
+			if ( null === $variables ) {
+				$variables = new Variables( $this->error_log );
+			}
+			$this->variables = $variables;
+		}
 
 		/**
 		 * User_Update::status_msg() is a stub function
@@ -34,45 +70,48 @@ if ( ! class_exists( '\E20R\Import_Members\Modules\Users' ) ) {
 		 * @param string $status
 		 * @param bool $allow_updates
 		 */
-		public static function status_msg( $status, $allow_updates ) {
+		public function status_msg( $status, $allow_updates ) {
 			// Not doing anything - stub for compatibility
 		}
 
 		/**
-		 * @param array $record
-		 * @param bool $allow_update
+		 * Validate the user information
 		 *
-		 * @return bool|string
+		 * @param array $record The record being updated/imported
+		 * @param bool|null $allow_update Whether the user/member info can be updated or not
+		 *
+		 * @return bool|int
+		 * @throws InvalidSettingsKey Thrown if the update_users parameter doesn't exist in the Variables() class
 		 */
-		public static function validate( $record, $allow_update ) {
+		public function validate( $record, $allow_update = null ) {
 
 			global $active_line_number;
 
-			$variables = new Variables();
-			$update    = (bool) $variables->get( 'update_users' );
+			if ( null === $allow_update ) {
+				$allow_update = (bool) $this->variables->get( 'update_users' );
+			}
 
 			if (
 				isset( $record['user_login'] ) &&
 				! empty( 'user_login' ) &&
-				false === $update &&
+				false === $allow_update &&
 				false !== get_user_by( 'login', $record['user_login'] )
 			) {
-				return 'user_login';
+				return Status::E20R_ERROR_NO_UPDATE_FROM_LOGIN;
 			}
 
 			if (
 				isset( $record['user_email'] ) &&
 				! empty( 'user_email' ) &&
-				false === $update &&
-				false !== get_user_by( 'login', $record['user_login'] )
+				false === $allow_update &&
+				false !== get_user_by( 'email', $record['user_email'] )
 			) {
-				return 'user_email';
+				return Status::E20R_ERROR_NO_UPDATE_FROM_EMAIL;
 			}
 
 			// BUG FIX: Not loading/updating record if user exists and the user identifiable data is the Email address
 			if ( ( empty( $user_data['user_login'] ) && empty( $user_data['user_email'] ) ) ) {
-
-				return 'user_login and user_email';
+				return Status::E20R_ERROR_NO_EMAIL_OR_LOGIN;
 			}
 
 			return true;
