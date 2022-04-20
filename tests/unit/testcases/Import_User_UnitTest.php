d64d6f832f573da46633ad819d0bbb82bfde954a
Thomas Sjolshagen <thomas@eighty20results.com>
2022-04-20 17:37:56 +0200

    BUG FIX: Add initial unit test for Import_User::import()

---
+++ tests/unit/testcases/Import_User_UnitTest.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * Copyright (c) 2022. - Eighty / 20 Results by Wicked Strong Chicks.
+ * ALL RIGHTS RESERVED
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package E20R\Tests\Unit\Email_Templates_UnitTest
+ */
+
+namespace E20R\Tests\Unit;
+
+use Codeception\Test\Unit;
+use E20R\Exceptions\InvalidInstantiation;
+use E20R\Exceptions\InvalidSettingsKey;
+use E20R\Import_Members\Data;
+use E20R\Import_Members\Email\Email_Templates;
+use E20R\Import_Members\Error_Log;
+use E20R\Import_Members\Import;
+use E20R\Import_Members\Modules\PMPro\Import_Member;
+use E20R\Import_Members\Modules\PMPro\PMPro;
+use E20R\Import_Members\Modules\Users\Import_User;
+use E20R\Import_Members\Process\Ajax;
+use E20R\Import_Members\Process\CSV;
+use E20R\Import_Members\Process\Page;
+use E20R\Import_Members\Validate_Data;
+use E20R\Import_Members\Variables;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Brain\Monkey;
+use Brain\Monkey\Functions;
+use WP_Mock;
+
+use stdClass;
+use MemberOrder;
+use WP_User;
+use function Brain\Monkey\Functions\stubs;
+
+class Import_User_UnitTest extends Unit {
+	use MockeryPHPUnitIntegration;
+
+	/**
+	 * Mocked Error_Log() class
+	 *
+	 * @var null|Error_Log
+	 */
+	private $mocked_errorlog = null;
+
+	/**
+	 * Mocked Variables() class
+	 *
+	 * @var null|Variables
+	 */
+	private $mocked_variables = null;
+
+	/**
+	 * Mocked Import() class
+	 *
+	 * @var null|Import
+	 */
+	private $mocked_import = null;
+
+	/**
+	 * Mocked PMPro MemberOrder()
+	 *
+	 * @var null|MemberOrder
+	 */
+	private $mock_order = null;
+
+	/**
+	 * Codeception setUp method
+	 *
+	 * @return void
+	 */
+	public function setUp() : void {
+		parent::setUp();
+		WP_Mock::setUp();
+		Monkey\setUp();
+		$this->load_stubs();
+		$this->load_mocks();
+	}
+
+	/**
+	 * Codeception tearDown() method
+	 *
+	 * @return void
+	 */
+	public function tearDown() : void {
+		WP_Mock::tearDown();
+		Monkey\tearDown();
+		parent::tearDown();
+	}
+
+	/**
+	 * Load all class mocks we need (with namespaces)
+	 */
+	public function load_mocks() : void {
+
+		$this->mocked_errorlog = $this->makeEmpty(
+			Error_Log::class,
+			array(
+				'debug' => function( $msg ) {
+					// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+					error_log( "Mocked log: {$msg}" );
+				},
+			)
+		);
+
+		$mocked_variables = $this->makeEmpty(
+			Variables::class
+		);
+
+		$mocked_pmpro = $this->makeEmpty(
+			PMPro::class
+		);
+
+		$mocked_data = $this->makeEmpty(
+			Data::class
+		);
+
+		$mocked_import_user = $this->makeEmpty(
+			Import_User::class
+		);
+
+		$mocked_import_member = $this->makeEmpty(
+			Import_Member::class
+		);
+
+		$mocked_csv = $this->makeEmpty(
+			CSV::class
+		);
+
+		$mocked_email_templates = $this->makeEmpty(
+			Email_Templates::class
+		);
+
+		$mocked_validate_data = $this->makeEmpty(
+			Validate_Data::class
+		);
+
+		$mocked_page = $this->makeEmpty(
+			Page::class
+		);
+
+		$mocked_ajax = $this->makeEmpty(
+			Ajax::class
+		);
+
+		$this->mocked_import = $this->constructEmpty(
+			Import::class,
+			array( $mocked_variables, $mocked_pmpro, $mocked_data, $mocked_import_user, $mocked_import_member, $mocked_csv, $mocked_email_templates, $mocked_validate_data, $mocked_page, $mocked_ajax, $this->mocked_errorlog ),
+			array(
+				'get' => function( $key ) use ( $mocked_variables ) {
+					if ( 'variables' === $key ) {
+						return $mocked_variables;
+					}
+					if ( 'error_log' === $key ) {
+						return $this->mocked_errorlog;
+					}
+
+					return null;
+				},
+			)
+		);
+	}
+
+	/**
+	 * Load all class mocks we need (with namespaces)
+	 *
+	 * @return void
+	 */
+	public function load_stubs() : void {
+
+		stubs(
+			array(
+				'__'                         => null,
+				'_e'                         => null,
+				'_ex'                        => null,
+				'_x'                         => null,
+				'_n'                         => null,
+				'_nx'                        => null,
+				'translate'                  => null,
+				'esc_html__'                 => null,
+				'esc_html_x'                 => null,
+				'esc_attr__'                 => null,
+				'esc_attr_x'                 => null,
+				'esc_html_e'                 => null,
+				'esc_attr_e'                 => null,
+				'get_transient'              => '/var/www/html/wp-content/uploads/e20r_import/example_file.csv',
+				'esc_url'                    => null,
+				'esc_url_raw'                => null,
+				'wp_upload_dir'              => function() {
+					return array(
+						'baseurl' => 'https://localhost:7537/wp-content/uploads/',
+						'basedir' => '/var/www/html/wp-content/uploads',
+					);
+				},
+				'register_deactivation_hook' => '__return_true',
+				'get_option'                 => 'https://www.paypal.com/cgi-bin/webscr',
+				'update_option'              => true,
+			)
+		);
+
+		Functions\when( 'plugin_dir_path' )
+			->justReturn( '/var/www/html/wp-content/plugins/pmpro-import-members-from-csv' );
+	}
+
+	/**
+	 * Test of the happy-path when importing a user who doesn't exist in the DB already
+	 *
+	 * @param int $user_line The user data to add during the import operation (line # in the inc/csv_files/user_data.csv file)
+	 * @param int $meta_line The metadata for the user being imported (line # in the inc/csv_files/meta_data.csv file)
+	 * @param int $expected Result we expect to see from the test execution
+	 *
+	 * @dataProvider fixture_create_user_import
+	 * @test
+	 */
+	public function it_should_create_new_user( $user_line, $meta_line, $expected ) {
+
+		$meta_headers = array();
+		$data_headers = array();
+		$import_data  = array();
+		$import_meta  = array();
+
+		// Read from one of the test file(s)
+		if ( null !== $user_line ) {
+			list( $import_data, $data_headers ) = fixture_read_from_user_csv( $user_line );
+		}
+
+		if ( null !== $meta_line ) {
+			list( $import_meta, $meta_headers ) = fixture_read_from_meta_csv( $meta_line );
+		}
+
+		$import_user = new Import_User( $this->mocked_variables, $this->mocked_errorlog );
+		$result      = $import_user->import( $import_data, $import_meta, ( $data_headers + $meta_headers ) );
+
+		self::assertSame( $expected, $result );
+	}
+
+	/**
+	 * Fixture generator for the it_should_create_new_user() test method
+	 *
+	 * @return array
+	 */
+	public function fixture_create_user_import() {
+		return array(
+			array(
+				1,
+				null,
+				array(
+					'user_login'   => 'test_user_1',
+					'user_email'   => 'test_user_1@example.com',
+					'user_pass'    => '',
+					'first_name'   => 'Thomas',
+					'last_name'    => 'Example',
+					'display_name' => 'Thomas Example',
+					'role'         => 'subscriber',
+				),
+			),
+		);
+	}
+
+	/**
+	 * Create mocked WP_User objects
+	 *
+	 * @param int $count The number of mocked WP_User objects to create and return
+	 *
+	 * @return array
+	 * @throws \Exception Thrown if we're unable to construct the mock WP_User object
+	 */
+	private function create_mocked_wp_users( $count = 1 ) {
+		$user_list = array();
+		$base_id   = 1000;
+
+		for ( $i = 0; $i < $count; $i++ ) {
+			$id = ( $base_id + $i );
+
+			if ( class_exists( '\WP_User' ) ) {
+				$user = $this->construct(
+					WP_User::class,
+					array( $id )
+				);
+			} else {
+				$user     = new stdClass();
+				$user->ID = $id;
+			}
+
+			$user->user_firstname   = 'Test';
+			$user->first_name       = $user->user_firstname;
+			$user->user_lastname    = "User # {$id}";
+			$user->last_name        = $user->user_lastname;
+			$user->user_description = "{$user->user_firstname} {$user->user_lastname}";
+			$user->display_name     = "{$user->user_firstname} {$user->user_lastname}";
+			$user->user_email       = "test_{$id}@localhost.local";
+
+			if ( $count > 1 ) {
+				$user_list[] = $user;
+			} else {
+				$user_list = $user;
+			}
+		}
+
+		return $user_list;
+	}
+}
