cd749df77915210af1e62422896c1d306e3bf980
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-05-29 16:21:59 +0200

    BUG FIX: Package definition (PHPDoc block in copyright notice)

--- tests/integration/testcases/Import_User_IntegrationTest.php
+++ tests/integration/testcases/Import_User_IntegrationTest.php
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @package E20R\Tests\Unit\Email_Templates_UnitTest
+ * @package E20R\Tests\Integration\Import_Users_IntegrationTest
  */
 
 namespace E20R\Tests\Integration;
