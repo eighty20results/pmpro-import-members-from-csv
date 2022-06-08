006d678ed934d2747a975cf1809efbf31b3dc1e7
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-06-08 07:54:43 +0200

    BUG FIX: Support refactored User_Present() class, clean up verify_import_file_path() method, rename extract_data() method to clean_data(), prepare to throw exceptions if meta or user data is empty after filtering when they shouldn't be, use e20r_import_wp_user_data filter to import/update WP_User data, support both error and warning logs

--- src/E20R/Import_Members/Process/CSV.php
+++ src/E20R/Import_Members/Process/CSV.php
@@ -20,12 +20,15 @@
 namespace E20R\Import_Members\Process;
 
 use E20R\Exceptions\InvalidSettingsKey;
+use E20R\Exceptions\NoUserDataFound;
+use E20R\Exceptions\NoUserMetadataFound;
 use E20R\Import_Members\Error_Log;
-use E20R\Import_Members\Modules\Users\Import_User;
-use E20R\Import_Members\Status;
+use E20R\Import_Members\Modules\Users\User_Present;
 use E20R\Import_Members\Variables;
 use SplFileObject;
 use WP_Error;
+
+use function \wp_upload_dir;
 
 if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 	/**
@@ -53,12 +56,19 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 		private $variables = null;
 
 		/**
+		 * Instance of the User_Present() class
+		 *
+		 * @var User_Present|null
+		 */
+		private $user_present = null;
+
+		/**
 		 * CSV constructor.
 		 *
 		 * @param Variables|null $variables Instance of the Request variables (settings) class
 		 * @param Error_Log|null $error_log For debug logging and status messages
 		 */
-		public function __construct( $variables = null, $error_log = null ) {
+		public function __construct( $variables = null, $error_log = null, $user_present = null ) {
 
 			if ( null === $error_log ) {
 				$error_log = new Error_Log(); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
@@ -69,6 +79,12 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				$variables = new Variables( $this->error_log );
 			}
 			$this->variables = $variables;
+
+			if ( null === $user_present ) {
+				$user_present = new User_Present( $this->variables, $this->error_log );
+			}
+
+			$this->user_present = $user_present;
 		}
 
 		/**
@@ -84,26 +100,23 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 		public function verify_import_file_path( $file_name = null, $import_dir_path = null ) {
 
 			// Only override the import directory when we're executing integration or unit tests
-			if ( null === $import_dir_path && ! defined( 'PLUGIN_INTEGRATION' ) && ! defined( 'PLUGIN_PHPUNIT' ) ) {
+			if ( null === $import_dir_path ) {
 				$upload_dir      = wp_upload_dir();
 				$import_dir_path = trailingslashit( $upload_dir['basedir'] ) . 'e20r_imports';
 			}
 
 			if ( empty( $file_name ) ) {
-				$this->error_log->debug( 'Loading file name from Variables()' );
 				$file_name = $this->variables->get( 'filename' );
 			}
 
 			if ( empty( $file_name ) ) {
-				$this->error_log->debug( 'Loading file name from transient' );
 				$file_name = get_transient( 'e20r_import_filename' );
 			}
 
-			// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			if ( empty( $file_name ) && ! empty( $_REQUEST['filename'] ) ) {
-				$this->error_log->debug( "Loading '{$_REQUEST['filename']}' from REQUEST variable" );
+			if ( empty( $file_name ) ) {
+				// Nonce handled in Ajax() class
 				// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-				$file_name = sanitize_file_name( $_REQUEST['filename'] );
+				$file_name = sanitize_file_name( $_REQUEST['filename'] ?? null );
 			}
 
 			// We enforce the location of the uploaded file (shouldn't be possible to fool us
@@ -116,7 +129,7 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 
 			$file_to_find = sprintf( '%1$s/%2$s', $import_dir_path, $file_name );
 
-			if ( false === file_exists( $file_to_find ) ) {
+			if ( empty( $import_dir_path ) || false === file_exists( $file_to_find ) ) {
 				$this->error_log->debug( 'File not found!' );
 				return false;
 			}
@@ -165,12 +178,10 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 			$this->variables->set( 'filename', $saved_filename );
 
 			if ( empty( $saved_filename ) && true === $background_import ) {
-
 				$this->error_log->add_error_msg(
 					esc_attr__( 'CSV file not selected. Nothing to Import!', 'pmpro-import-members-from-csv' ),
 					'error'
 				);
-
 				$no_file_error = true;
 			}
 
@@ -178,7 +189,6 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 			$destination_name = "{$filename}";
 
 			if ( ! is_dir( $import_dir ) && false === wp_mkdir_p( $import_dir ) ) {
-
 				$this->error_log->add_error_msg(
 					sprintf(
 					// translators: %s Directory for the CSV Import file
@@ -192,13 +202,11 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				);
 
 				$this->error_log->debug( "Unable to create or find the Import directory: {$import_dir}" );
-
 				return false;
 			}
 
 			// Save the uploaded file
 			if ( false !== strpos( $_FILES['members_csv']['tmp_name'], $upload_dir['basedir'] ) ) {
-
 				// Was uploaded and saved to $_SESSION
 				rename( $_FILES['members_csv']['tmp_name'], $destination_name );
 				$this->error_log->debug( ( "Renamed {$_FILES['members_csv']['tmp_name']} to {$destination_name}" ) );
@@ -216,7 +224,6 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 			// @phpstan-ignore-next-line
 			if ( ( true === $no_file_error || true === $directory_error || true === $clean_file_error ) ) {
 				$this->error_log->debug( 'Error: Problem with uploaded file...' );
-
 				return false;
 			}
 
@@ -308,14 +315,17 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 		 *
 		 * @param string           $file_name Name of the CSV file we're processing
 		 * @param array            $args File arguments supplied
-		 * @param Import_User|null $import_user  Optional Import_User() class instance
 		 * @param SplFileObject|null $file_object Optional SplFileObject() class instance
 		 *
 		 * @return array
 		 *
+		 * @throws NoUserDataFound Thrown if one of the filter handlers returns an empty array of user data
+		 * @throws NoUserMetadataFound Thrown when one of the filter handlers returns and empty array of user metadata
+		 * @throws InvalidSettingsKey Thrown if the property specified in the Variable::get() doesn't exist
+		 *
 		 * @since 0.5
 		 */
-		public function process( $file_name, $args, $import_user = null, $file_object = null ) {
+		public function process( $file_name, $args, $file_object = null ) {
 
 			global $active_line_number;
 			global $e20r_import_err;
@@ -331,12 +341,7 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				$e20r_import_warn = array();
 			}
 
-			$headers = array();
-
-			if ( null === $import_user ) {
-				$import_user = new Import_User( $this->variables, $this->error_log, $this->user_presence );
-			}
-
+			$headers  = array();
 			$user_ids = array();
 			$defaults = apply_filters( 'pmp_im_import_default_settings', $this->variables->get_defaults() );
 			$defaults = apply_filters( 'e20r_import_default_settings', $defaults );
@@ -392,6 +397,8 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				$active_line_number++;
 				$error_key = null;
 
+				$this->error_log->debug( "Active line # is now: {$active_line_number}" );
+
 				// Read a line from the file and remove the BOM character
 				$line = $file_object->fgetcsv();
 
@@ -399,9 +406,10 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				// If another line is empty, just skip it
 				if ( empty( $line ) ) {
 					if ( true === $first ) {
-						$msg = esc_attr__( 'The expected header line in the Import file is missing?!?', 'pmpro-import-members-from-csv' );
+						$msg = esc_attr__( 'Missing header line in the CSV file being imported!', 'pmpro-import-members-from-csv' );
 						$this->error_log->add_error_msg( $msg, 'error' );
 						$e20r_import_err[ "header_missing_{$active_line_number}" ] = new WP_Error( 'e20r_im_header', $msg );
+						$this->error_log->debug( $msg );
 						break;
 					}
 					$this->error_log->debug( "Line # {$active_line_number} is empty" );
@@ -415,7 +423,6 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 
 					// Skip ahead ?
 					if ( ! empty( $partial ) ) {
-
 						// Get filename only
 						$active_line_number = get_option( "e20rcsv_{$file}", null );
 
@@ -437,7 +444,7 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 
 				$this->error_log->debug( "Processing next user data. (previous line #: {$active_line_number})" );
 				try {
-					$this->extract_data(
+					$this->clean_data(
 						$line,
 						$user_data,
 						$user_meta,
@@ -456,91 +463,6 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 					continue;
 				}
 
-				$this->error_log->debug( "Processed line #{$active_line_number}..." );
-				$user_id_status = apply_filters( 'e20r_import_users_validate_field_data', false, null, $user_data );
-
-				// TODO: Process cached warning messages as well
-
-				switch ( $user_id_status ) {
-					case Status::E20R_ERROR_NO_USER_ID:
-						$msg = sprintf(
-						// translators: %1$d - Line number in the CSV file being imported
-							esc_attr__(
-								'Missing ID, user_login and/or user_email information column at row %1$d',
-								'pmpro-import-members-from-csv'
-							),
-							$active_line_number
-						);
-
-						$error_key = "user_id_missing_{$active_line_number}";
-						break;
-					case Status::E20R_ERROR_USER_NOT_FOUND:
-						$msg = sprintf(
-						// translators: %1$d - User ID, %2$d - Current line number in the CSV file being imported
-							esc_attr__(
-								'WP User ID %1$d not found in database (from CSV file line: %2$d)',
-								'pmpro-import-members-from-csv'
-							),
-							$user_data['ID'],
-							$active_line_number
-						);
-						$error_key = "user_missing_{$active_line_number}";
-						break;
-
-					case Status::E20R_ERROR_NO_UPDATE_FROM_LOGIN:
-						$msg = sprintf(
-						// translators: %1$d - User's user_login info, %2$d - Current line number in the CSV file being imported
-							esc_attr__(
-								'User login %1$d already exists, updates are disallowed (from CSV file line: %2$d)',
-								'pmpro-import-members-from-csv'
-							),
-							$user_data['user_login'],
-							$active_line_number
-						);
-						$warning_key = "existing_user_login_{$active_line_number}";
-						break;
-
-					case Status::E20R_ERROR_NO_UPDATE_FROM_EMAIL:
-						$msg = sprintf(
-						// translators: %1$d - User's user_email, %2$d - Current line number in the CSV file being imported
-							esc_attr__(
-								'User (%1$d) already exists, updates are disallowed (from CSV file line: %2$d)',
-								'pmpro-import-members-from-csv'
-							),
-							$user_data['user_email'],
-							$active_line_number
-						);
-						$warning_key = "existing_user_email_{$active_line_number}";
-						break;
-
-					case Status::E20R_ERROR_NO_EMAIL_OR_LOGIN:
-						$msg = sprintf(
-						// translators: %1$d - User's user_email, %2$d - Current line number in the CSV file being imported
-							esc_attr__(
-								'Neither user_email, nor user_login information provided (from CSV file line: %1$d)',
-								'pmpro-import-members-from-csv'
-							),
-							$active_line_number
-						);
-						$error_key = "user_email_missing_{$active_line_number}";
-						break;
-				}
-
-				if ( true !== $user_id_status && ! empty( $msg ) ) {
-					if ( ! empty( $error_key ) ) {
-						$e20r_import_err[ $error_key ] = new WP_Error( 'e20r_im_missing_data', $msg );
-					}
-					if ( ! empty( $warning_key ) ) {
-						$e20r_import_warn[ $error_key ] = new WP_Error( 'e20r_im_missing_data', $msg );
-					}
-					$this->error_log->debug( $msg );
-					$msg = null;
-				}
-
-				if ( ! empty( $msg ) ) {
-					$this->error_log->add_error_msg( $msg, 'error' );
-				}
-
 				// A plugin may need to filter the data and meta
 				$user_data = apply_filters( 'is_iu_import_userdata', $user_data, $user_meta, $settings );
 				$user_data = apply_filters( 'pmp_im_import_userdata', $user_data, $user_meta, $settings );
@@ -549,34 +471,25 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 				$user_meta = apply_filters( 'pmp_im_import_usermeta', $user_meta, $user_data, $settings );
 				$user_meta = apply_filters( 'e20r_import_usermeta', $user_meta, $user_data, $headers );
 
-				// If no user data, bailout!
+				// The *_import_userdata filter did something unexpected
 				if ( empty( $user_data ) ) {
-					$msg = sprintf(
-					// translators: %d - Line number in the CSV file being imported
-						esc_attr__( 'No user data found at row #%d', 'pmpro-import-members-from-csv' ),
-						( $active_line_number + 1 )
-					);
+					$this->error_log->debug( 'Empty user data from CSV file!?!' );
+					// FIXME: throw new NoUserDataFound();
+				}
 
-					$e20r_import_warn[ "warning_userdata_{$active_line_number}" ] = new WP_Error( 'e20r_im_nodata', $msg );
+				// The *_import_usermeta filter did something unexpected
+				if ( empty( $user_meta ) ) {
+					$this->error_log->debug( 'Empty user metadata from CSV file!?!' );
+					// FIXME: throw new NoUserMetadataFound();
+				}
 
-					$this->error_log->debug( $msg );
-				} else {
+				$user_record_valid = apply_filters( 'e20r_import_users_validate_field_data', false, null, $user_data );
 
-					$this->error_log->debug( 'Importing user data' );
-					// Try to Import user record and trigger other Import Modules
-					try {
-						$user_id = $import_user->import( $user_data, $user_meta, $headers );
-					} catch ( InvalidSettingsKey $e ) {
-						$msg = sprintf(
-						// translators: %1$d - Line number in the CSV file being imported
-							esc_attr__( 'Cannot import user data from row %1$d', 'pmpro-import-members-from-csv' ),
-							$active_line_number
-						);
+				$this->error_log->debug( "Processed line #{$active_line_number} and user ID existence status: '{$user_record_valid}': " . ( $user_record_valid ? $user_record_valid : 'False' ) );
 
-						$this->error_log->add_error_msg( $msg, 'error' );
-						$this->error_log->debug( $msg );
-						$e20r_import_err[ "user_data_missing_{$active_line_number}" ] = new WP_Error( 'e20r_im_missing_data', $msg );
-					}
+				if ( true === $user_record_valid ) {
+					// Using a filter to trigger an add, or update for the actual WordPress user
+					$user_id = apply_filters( 'e20r_import_wp_user_data', $user_data, $user_meta, $headers, $settings );
 
 					/** BUG FIX: Didn't save the created user's ID and added empty user IDs*/
 					if ( null !== $user_id ) {
@@ -597,16 +510,23 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 			$file_object = null;
 			ini_set( 'auto_detect_line_endings', '1' );
 
-			// One more thing to do after all imports?
+			// More things to do after the user record was imported?
 			do_action( 'is_iu_post_users_import', $user_ids, $e20r_import_err );
 			do_action( 'pmp_im_post_members_import', $user_ids, $e20r_import_err );
 			do_action( 'e20r_import_post_members', $user_ids, $e20r_import_err );
 
+			// Let's log the warning messages
+			$this->error_log->log_errors(
+				$e20r_import_warn,
+				$this->variables->get( 'logfile_warning_path' ),
+				$this->variables->get( 'logfile_warning_url' )
+			);
+
 			// Let's log the errors
 			$this->error_log->log_errors(
-				array_merge( $e20r_import_err, $e20r_import_warn ),
-				$this->variables->get( 'logfile_path' ),
-				$this->variables->get( 'logfile_url' )
+				$e20r_import_err,
+				$this->variables->get( 'logfile_error_path' ),
+				$this->variables->get( 'logfile_error_url' )
 			);
 
 			// Return to the active (pre Import) site
@@ -674,7 +594,7 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 		}
 
 		/**
-		 * Extract CSV data into correct location (column/value pairs)
+		 * Clean CSV data and place at the correct location (column/value pairs)
 		 *
 		 * @param string[] $line - Line of data from the CSV file we're processing
 		 * @param array $user_data - User data to Import (by reference)
@@ -682,7 +602,7 @@ if ( ! class_exists( '\E20R\Import_Members\Process\CSV' ) ) {
 		 * @param array $headers
 		 * @param array $user_data_fields
 		 */
-		private function extract_data( $line, &$user_data, &$user_meta, $headers, $user_data_fields ) {
+		private function clean_data( $line, &$user_data, &$user_meta, $headers, $user_data_fields ) {
 
 			global $e20r_import_err;
 			global $active_line_number;
