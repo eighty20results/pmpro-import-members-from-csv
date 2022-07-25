354df0e558c0b9a782e8147f0cb177230d0f3845
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-07-25 14:15:32 +0200

    BUG FIX: Refactor Unit and Integration testing for Import_User() class

--- tests/integration/testcases/Import_User_IntegrationTest.php
+++ tests/integration/testcases/Import_User_IntegrationTest.php
@@ -34,6 +34,7 @@ use E20R\Tests\Fixtures\Factory\E20R_IntegrationTest_Generator_Sequence;
 use E20R\Tests\Fixtures\Factory\E20R_TestCase;
 use E20R\Tests\Integration\Fixtures\Manage_Test_Data;
 
+use Mockery;
 use WP_Mock;
 use org\bovigo\vfs\vfsStream;
 
@@ -41,8 +42,17 @@ use MemberOrder;
 
 use WP_Error;
 use WP_User;
+use wpdb;
+
+use Brain\Monkey;
+use Brain\Monkey\Functions;
+use function Brain\Monkey\Functions\expect;
+use function Brain\Monkey\Functions\stubs;
 
 class Import_User_IntegrationTest extends E20R_TestCase {
+
+	use \Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
 
 	/**
 	 * Default Error_Log() class
@@ -128,6 +138,7 @@ class Import_User_IntegrationTest extends E20R_TestCase {
 	 */
 	public function setUp(): void {
 		parent::setUp();
+		Monkey\setUp();
 		WP_Mock::setUp();
 
 		$this->load_mocks();
@@ -157,6 +168,7 @@ class Import_User_IntegrationTest extends E20R_TestCase {
 	 * @return void
 	 */
 	public function tearDown(): void {
+		Monkey\tearDown();
 		WP_Mock::tearDown();
 		parent::tearDown();
 	}
@@ -666,7 +678,7 @@ class Import_User_IntegrationTest extends E20R_TestCase {
 	 * @test
 	 * @dataProvider fixture_deprecated_actions
 	 */
-	public function it_should_print_messages( $action_name, $expected_message ) {
+	public function it_should_print_deprecated_messages( $action_name, $expected_message ) {
 
 		global $e20r_test_result_msg;
 		$e20r_test_result_msg = null;
@@ -717,137 +729,5 @@ class Import_User_IntegrationTest extends E20R_TestCase {
 		);
 	}
 
-	/**
-	 * Tests the Import_User::import_usermeta() method
-	 *
-	 * @param array $user_meta The supplied user metadata fields with values
-	 * @param array $expected The expected key/value pairs being returned from import_usermeta()
-	 *
-	 * @return void
-	 * @throws \Exception
-	 *
-	 * @test
-	 * @dataProvider fixture_user_meta_and_fields
-	 */
-	public function it_should_append_imported_to_processed_array_keys( $user_meta, $expected ) {
 
-		$mock_errlog = $this->makeEmpty(
-			Error_Log::class,
-			array(
-				'debug' => function( $message ) {
-					// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-					error_log( 'Mocked: ' . $message );
-				},
-			)
-		);
-
-		$m_present = $this->makeEmpty( User_Present::class );
-		$m_passwd  = $this->makeEmpty( Generate_Password::class );
-		$m_vars    = $this->makeEmpty(
-			Variables::class,
-			array(
-				'get' => $expected,
-			)
-		);
-		$m_time    = $this->makeEmpty( Time::class );
-		$m_date    = $this->makeEmpty( Date_Format::class );
-
-		$import_user   = new Import_User( $m_vars, $mock_errlog, $m_present, $m_passwd, $m_time, $m_date );
-		$returned_meta = $import_user->import_usermeta( $user_meta, array(), array() );
-
-		self::assertIsArray( $returned_meta );
-
-		if ( is_array( $user_meta ) ) {
-			foreach ( $user_meta as $meta_key => $value ) {
-				$has_both_keys =
-					in_array( "imported_{$meta_key}", array_keys( $returned_meta ), true ) &&
-					in_array( $meta_key, array_keys( $returned_meta ), true );
-				self::assertTrue( $has_both_keys );
-			}
-		}
-	}
-
-	/**
-	 * User meta field array fixture
-	 *
-	 * @return array
-	 */
-	public function fixture_user_meta_and_fields() {
-		$pmpro_fields = array(
-			'membership_id'                          => null,
-			'membership_code_id'                     => null,
-			'membership_discount_code'               => null,
-			'membership_initial_payment'             => null,
-			'membership_billing_amount'              => null,
-			'membership_cycle_number'                => null,
-			'membership_cycle_period'                => null,
-			'membership_billing_limit'               => null,
-			'membership_trial_amount'                => null,
-			'membership_trial_limit'                 => null,
-			'membership_status'                      => null,
-			'membership_startdate'                   => null,
-			'membership_enddate'                     => null,
-			'membership_subscription_transaction_id' => null,
-			'membership_payment_transaction_id'      => null,
-			'membership_gateway'                     => null,
-			'membership_affiliate_id'                => null,
-			'membership_timestamp'                   => null,
-		);
-		$buddypress   = array(
-			'bp_profile' => null,
-			'bp_group'   => null,
-		);
-
-		return array(
-			array( $pmpro_fields + $buddypress, $pmpro_fields + $buddypress ),
-			array( $buddypress, $buddypress ),
-			array( $pmpro_fields, $pmpro_fields ),
-			array( null, array() ),
-			array( false, array() ),
-			array( 'something weird', array() ),
-			array( array(), array() ),
-			array( [], array() ), // phpcs:ignore Generic.Arrays.DisallowShortArraySyntax.Found
-		);
-	}
-
-	/**
-	 * InvalidProperty exception thrown and caught if (for some weird reason) the 'fields' property is missing
-	 * from the Variables() class. The supplied user_meta array should not be processed and there should
-	 * NOT be any keys with an 'imported_' prefix in the returned array
-	 *
-	 * @return void
-	 * @throws \Exception
-	 *
-	 * @test
-	 * @dataProvider fixture_user_meta_and_fields
-	 */
-	public function it_should_throw_invalid_property_exception( $user_meta ) {
-		$mock_errlog = $this->makeEmpty(
-			Error_Log::class,
-			array(
-				'debug' => function( $message ) {
-					// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-					error_log( 'Mocked: ' . $message );
-				},
-			)
-		);
-
-		$m_present = $this->makeEmpty( User_Present::class );
-		$m_passwd  = $this->makeEmpty( Generate_Password::class );
-		$m_vars    = $this->getMockBuilder( Variables::class )->getMock();
-		$m_vars->method( 'get' )
-				->willThrowException(
-					new InvalidProperty(
-						'Error: The requested parameter \'fields\' does not exist!'
-					)
-				);
-		$m_time        = $this->makeEmpty( Time::class );
-		$m_date        = $this->makeEmpty( Date_Format::class );
-		$import_user   = new Import_User( $m_vars, $mock_errlog, $m_present, $m_passwd, $m_time, $m_date );
-		$returned_meta = $import_user->import_usermeta( $user_meta, array(), array() );
-		self::assertIsArray( $returned_meta );
-		foreach ( $returned_meta as $key => $value ) {
-			self::assertArrayNotHasKey( "imported_{$key}", $returned_meta );
-		}
-	}
 }
--- tests/unit/testcases/Import_User_UnitTest.php
+++ tests/unit/testcases/Import_User_UnitTest.php
@@ -22,6 +22,7 @@
 namespace E20R\Tests\Unit;
 
 use E20R\Exceptions\CannotUpdateDB;
+use E20R\Exceptions\InvalidProperty;
 use E20R\Exceptions\UserIDAlreadyExists;
 use E20R\Import_Members\Error_Log;
 use E20R\Import_Members\Import;
@@ -33,6 +34,7 @@ use E20R\Import_Members\Validate\Time;
 use E20R\Import_Members\Variables;
 
 use Codeception\Test\Unit;
+use E20R\Tests\Integration\Fixtures\Manage_Test_Data;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Brain\Monkey;
 use Brain\Monkey\Functions;
@@ -42,6 +44,8 @@ use WP_Mock;
 
 use MemberOrder;
 use WP_User;
+use wpdb;
+use function Brain\Monkey\Functions\expect;
 
 class Import_User_UnitTest extends Unit {
 	use MockeryPHPUnitIntegration;
@@ -97,14 +101,22 @@ class Import_User_UnitTest extends Unit {
 	private $mocked_date_validator = null;
 
 	/**
+	 * The path to the file containing the user data
+	 *
+	 * @var null|string $fixture_user_csv
+	 */
+	private $fixture_user_csv = null;
+
+	/**
 	 * Codeception setUp method
 	 *
 	 * @return void
 	 */
 	public function setUp() : void {
 		parent::setUp();
-		WP_Mock::setUp();
 		Monkey\setUp();
+
+		$this->fixture_user_csv = __DIR__ . '/../../_data/csv_files/fixture_user_data.csv';
 		$this->load_mocks();
 	}
 
@@ -114,7 +126,6 @@ class Import_User_UnitTest extends Unit {
 	 * @return void
 	 */
 	public function tearDown() : void {
-		WP_Mock::tearDown();
 		Monkey\tearDown();
 		parent::tearDown();
 	}
@@ -145,7 +156,7 @@ class Import_User_UnitTest extends Unit {
 				array(
 					'debug' => function( $msg ) {
 						// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped, WordPress.PHP.DevelopmentFunctions.error_log_error_log
-						error_log( 'Mocked debug: ' . $msg );
+						error_log( 'Mocked: ' . $msg );
 
 					},
 				)
@@ -232,8 +243,6 @@ class Import_User_UnitTest extends Unit {
 	 * @test
 	 */
 	public function it_should_throw_exception( $user, $user_return, $data, $exception ) {
-		Functions\expect( 'get_user_by' )->once()->andReturn( $user_return );
-
 		$m_variables = $this->makeEmpty(
 			Variables::class,
 			array(
@@ -250,6 +259,8 @@ class Import_User_UnitTest extends Unit {
 		$mocked_date_validator   = $this->makeEmpty( Date_Format::class );
 		$mocked_passwd_validator = $this->makeEmpty( Generate_Password::class );
 		$mocked_variables        = $this->makeEmpty( Variables::class );
+
+		Functions\expect( 'get_userdata' )->once()->andReturn( $user_return );
 
 		$mocked_user_present_validator = $this->constructEmpty(
 			User_Present::class,
@@ -305,5 +316,227 @@ class Import_User_UnitTest extends Unit {
 				CannotUpdateDB::class,
 			),
 		);
+	}
+
+	/**
+	 * Tests the Import_User::import_usermeta() method
+	 *
+	 * @param array $user_meta The supplied user metadata fields with values
+	 * @param array $expected The expected key/value pairs being returned from import_usermeta()
+	 *
+	 * @return void
+	 * @throws \Exception
+	 *
+	 * @test
+	 * @dataProvider fixture_user_meta_and_fields
+	 */
+	public function it_should_append_imported_to_processed_array_keys( $user_meta, $expected ) {
+		$m_present = $this->makeEmpty( User_Present::class );
+		$m_passwd  = $this->makeEmpty( Generate_Password::class );
+		$m_vars    = $this->makeEmpty(
+			Variables::class,
+			array(
+				'get' => $expected,
+			)
+		);
+		$m_time    = $this->makeEmpty( Time::class );
+		$m_date    = $this->makeEmpty( Date_Format::class );
+
+		$import_user   = new Import_User( $m_vars, $this->mocked_errorlog, $m_present, $m_passwd, $m_time, $m_date );
+		$returned_meta = $import_user->import_usermeta( $user_meta, array(), array() );
+
+		self::assertIsArray( $returned_meta );
+
+		if ( is_array( $user_meta ) ) {
+			foreach ( $user_meta as $meta_key => $value ) {
+				$has_both_keys =
+					in_array( "imported_{$meta_key}", array_keys( $returned_meta ), true ) &&
+					in_array( $meta_key, array_keys( $returned_meta ), true );
+				self::assertTrue( $has_both_keys );
+			}
+		}
+	}
+
+	/**
+	 * User meta field array fixture
+	 *
+	 * @return array
+	 */
+	public function fixture_user_meta_and_fields() {
+		$pmpro_fields = array(
+			'membership_id'                          => null,
+			'membership_code_id'                     => null,
+			'membership_discount_code'               => null,
+			'membership_initial_payment'             => null,
+			'membership_billing_amount'              => null,
+			'membership_cycle_number'                => null,
+			'membership_cycle_period'                => null,
+			'membership_billing_limit'               => null,
+			'membership_trial_amount'                => null,
+			'membership_trial_limit'                 => null,
+			'membership_status'                      => null,
+			'membership_startdate'                   => null,
+			'membership_enddate'                     => null,
+			'membership_subscription_transaction_id' => null,
+			'membership_payment_transaction_id'      => null,
+			'membership_gateway'                     => null,
+			'membership_affiliate_id'                => null,
+			'membership_timestamp'                   => null,
+		);
+		$buddypress   = array(
+			'bp_profile' => null,
+			'bp_group'   => null,
+		);
+
+		return array(
+			array( $pmpro_fields + $buddypress, $pmpro_fields + $buddypress ),
+			array( $buddypress, $buddypress ),
+			array( $pmpro_fields, $pmpro_fields ),
+			array( null, array() ),
+			array( false, array() ),
+			array( 'something weird', array() ),
+			array( array(), array() ),
+			array( [], array() ), // phpcs:ignore Generic.Arrays.DisallowShortArraySyntax.Found
+		);
+	}
+
+	/**
+	 * InvalidProperty exception thrown and caught if (for some weird reason) the 'fields' property is missing
+	 * from the Variables() class. The supplied user_meta array should not be processed and there should
+	 * NOT be any keys with an 'imported_' prefix in the returned array
+	 *
+	 * @return void
+	 * @throws \Exception
+	 *
+	 * @test
+	 * @dataProvider fixture_user_meta_and_fields
+	 */
+	public function it_should_throw_invalid_property_exception( $user_meta ) {
+		$m_present = $this->makeEmpty( User_Present::class );
+		$m_passwd  = $this->makeEmpty( Generate_Password::class );
+		$m_vars    = $this->getMockBuilder( Variables::class )->getMock();
+		$m_vars->method( 'get' )
+			->willThrowException(
+				new InvalidProperty(
+					'Error: The requested parameter \'fields\' does not exist!'
+				)
+			);
+		$m_time        = $this->makeEmpty( Time::class );
+		$m_date        = $this->makeEmpty( Date_Format::class );
+		$import_user   = new Import_User( $m_vars, $this->mocked_errorlog, $m_present, $m_passwd, $m_time, $m_date );
+		$returned_meta = $import_user->import_usermeta( $user_meta, array(), array() );
+		self::assertIsArray( $returned_meta );
+		foreach ( $returned_meta as $key => $value ) {
+			self::assertArrayNotHasKey( "imported_{$key}", $returned_meta );
+		}
+	}
+
+	/**
+	 * Tests Import_User::update_user_id()
+	 *
+	 * @param int $line_no The line number we use from the fixture_user_csv file
+	 * @param bool $update_id Whether to allow updating the User's ID in the database
+	 * @param bool $wpdb_update_returns The value we want $wpdb->update() to return
+	 * @param string $user_table Table name we're pretending to use
+	 * @param array $csv_user_data Data we're pretending to import
+	 * @param string|null $exception_thrown The exception we're expecting to see thrown by update_user_id()
+	 *
+	 * @return void
+	 * @throws InvalidProperty
+	 * @throws \E20R\Exceptions\CannotUpdateDB
+	 * @throws \E20R\Exceptions\UserIDAlreadyExists
+	 *
+	 * @test
+	 * @dataProvider fixture_update_user_id_tests
+	 */
+	public function it_doesnt_update_the_db_since_we_found_the_existing_user( $line_no, $update_id, $wpdb_update_returns, $user_table, $csv_user_data, $exception_thrown ) {
+		global $wpdb;
+		global $active_line_number;
+
+		$active_line_number = $line_no;
+		$user               = $this->fixture_create_wp_user( $line_no );
+
+		$new_wpdb = $this->makeEmpty(
+			wpdb::class,
+			array(
+				'update' => $wpdb_update_returns,
+			)
+		);
+
+		$new_wpdb->pmpro_membership_levels = 'wp_pmpro_membership_levels';
+		$new_wpdb->users                   = $user_table;
+
+		$m_present = $this->makeEmpty( User_Present::class );
+		$m_passwd  = $this->makeEmpty( Generate_Password::class );
+		$m_vars    = $this->makeEmpty(
+			Variables::class,
+			array(
+				'get' => function( $param ) use ( $update_id ) {
+					if ( '' === $param ) {
+						return $update_id;
+					}
+					return null;
+				},
+			)
+		);
+		$m_time    = $this->makeEmpty( Time::class );
+		$m_date    = $this->makeEmpty( Date_Format::class );
+
+		if ( null !== $exception_thrown ) {
+			$this->expectException( $exception_thrown );
+		}
+
+		expect( 'get_userdata' )
+			->once()
+			->with( $csv_user_data['ID'] )
+			->andReturn( $user );
+		expect( 'get_user_by' )
+			->once()
+			->with( 'ID', $csv_user_data['ID'] )
+			->andReturn( $user );
+
+		$import_user = new Import_User( $m_vars, $this->mocked_errorlog, $m_present, $m_passwd, $m_time, $m_date );
+
+		// Switch to mocked WPDB object and then run test
+		$orig_wpdb = $wpdb;
+		$wpdb      = $new_wpdb; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$result    = $import_user->update_user_id( $user, $csv_user_data );
+		$wpdb      = $orig_wpdb; // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+
+		self::assertSame( $user->ID, $result->ID );
+		self::assertInstanceOf( WP_User::class, $result );
+	}
+
+	/**
+	 * Fixture to validate behavior when supplied user object and ID being imported (from CSV data) is the same
+	 *
+	 * @return array[]
+	 */
+	public function fixture_update_user_id_tests() {
+		return array(
+			// line no, allow update of User's IDin DB, wpdb->update returns, user table name, import data array, $thrown exception
+			array( 0, false, true, 'wp_users', array( 'ID' => 1023 ), null ),
+			array( 1, false, true, 'wp_users', array( 'ID' => 65536 ), null ),
+			array( 2, true, true, 'wptest_users', array( 'ID' => 1111 ), null ),
+		);
+	}
+	/**
+	 * Generate a mocked WP_User object with data from the integrations/data/fixture_user_data.csv file
+	 *
+	 * @param int $line_no The line to read from the CSV file
+	 *
+	 * @return \PHPUnit\Framework\MockObject\MockObject|WP_User
+	 * @throws \Exception
+	 */
+	public function fixture_create_wp_user( $line_no ) {
+		$data                        = new Manage_Test_Data();
+		list( $headers, $user_data ) = $data->read_line_from_csv( $line_no, $this->fixture_user_csv );
+		$m_user                      = new WP_User();
+
+		foreach ( $user_data as $property => $value ) {
+			$m_user->{$property} = $value;
+		}
+
+		return $m_user;
 	}
 }
