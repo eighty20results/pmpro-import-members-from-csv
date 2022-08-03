31b9caa53b7be47d2c736d51ac342ac853920604
Eighty/20Results Bot on Github <thomas@eighty20results.com>
2022-08-03 07:48:51 +0200

    BUG FIX: Time out looking for the DB container if it's not available

--- bin/wait-for-db.sh
+++ bin/wait-for-db.sh
@@ -3,13 +3,18 @@ WORDPRESS_DB_USER="${1}"
 WORDPRESS_DB_PASSWORD="${2}"
 WORDPRESS_DB_HOST="${3}"
 E20R_PLUGIN_NAME="${4}"
-
+COUNTER=0
 until docker container exec \
 	"mariadb-wp-${E20R_PLUGIN_NAME}" \
 	mysqladmin ping -P 3306 -p"${WORDPRESS_DB_PASSWORD}" -u"${WORDPRESS_DB_USER}" -h"${WORDPRESS_DB_HOST}" | \
 	grep "mysqld is alive" ; do
   >&2 echo "MySQL is unavailable - waiting for it... 😴"
   sleep 2
+  ((COUNTER=COUNTER+1))
+  if [[ $COUNTER -gt 10 ]]; then
+		echo "Unable to locate working instance of DB container! Exiting!"
+		exit 1
+  fi
 done
 echo "Standby to let Docker stack for ${E20R_PLUGIN_NAME} testing settle a bit"
 sleep 5
